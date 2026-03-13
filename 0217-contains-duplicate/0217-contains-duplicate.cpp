709114803c8a51fc5ea531a263c63d160132aa48
adityagautam8960-debug <adityagautam8960@gmail.com>
2026-03-13 22:23:44 +0530

    Time: 0 ms (100%), Space: 71 MB (78.4%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,22 +1,21 @@
-#include <vector>
-#include <unordered_set>
-
 class Solution {
 public:
-    bool containsDuplicate(std::vector<int>& nums) {
-        // Unordered set duplicate allow nahi karta aur search O(1) mein karta hai
-        std::unordered_set<int> seen;
-        
-        for (int num : nums) {
-            // Agar number pehle se set mein maujood hai
-            if (seen.count(num)) {
-                return true;
+    bool containsDuplicate(vector<int>& nums){
+        int n = nums.size() - 1;
+        for (int i = 0; i < n; ++i)
+        {
+            int swapped = false;
+            for (int j = 0; j < n - i; ++j)
+            {
+                if (nums[j] == nums[j + 1]) return true;
+                if (nums[j] > nums[j + 1])
+                {
+                    swapped = true;
+                    std::swap(nums[j], nums[j + 1]);
+                }
             }
-            // Warna number ko set mein insert karo
-            seen.insert(num);
+            if (!swapped) return false;
         }
-        
-        // Agar poora loop chal gaya aur koi duplicate nahi mila
         return false;
     }
 };

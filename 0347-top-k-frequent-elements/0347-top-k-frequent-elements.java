64559893dfd7753b92a044e6199c710d6b68ccbe
adityagautam8960-debug <adityagautam8960@gmail.com>
2026-03-17 22:07:30 +0530

    Time: 0 ms (100%), Space: 47.3 MB (94.3%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,35 +1,47 @@
-import java.util.*;
-
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        // Step 1: Count frequency of each number using HashMap
-        Map<Integer, Integer> freqMap = new HashMap<>();
-        for (int num : nums) {
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+    public static int[] topKFrequent(int[] nums, int k) {
+
+  int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int num:nums)
+        {
+            if(num>max)max=num;
+            if(num<min)min=num;
         }
-        
-        // Step 2: Create buckets where index = frequency, value = list of numbers
-        List<Integer>[] buckets = new List[nums.length + 1];
-        for (int i = 0; i < buckets.length; i++) {
-            buckets[i] = new ArrayList<>();
+        int[] freq=new int[max-min+1];
+        int highestFreq=0;
+        for(int i:nums)
+        {
+            int indx=i-min;
+            int count=++freq[indx];
+            if(count>highestFreq)highestFreq=count;
         }
-        
-        // Step 3: Fill buckets
-        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
-            int num = entry.getKey();
-            int freq = entry.getValue();
-            buckets[freq].add(num);
+        ArrayList<Integer>[] bucketList=new ArrayList[highestFreq];
+        for(int i=0;i<freq.length;i++)
+        {
+            int freqIndex =freq[i]-1;
+            if(freqIndex ==-1)continue;
+            if(bucketList[freqIndex]==null)
+                bucketList[freqIndex ]=new ArrayList<>();
+           bucketList[freqIndex ].add(i+min);
         }
-        
-        // Step 4: Collect k most frequent elements from highest frequency buckets
-        List<Integer> result = new ArrayList<>();
-        for (int i = buckets.length - 1; i >= 0 && result.size() < k; i--) {
-            if (!buckets[i].isEmpty()) {
-                result.addAll(buckets[i]);
+        int[] result=new int[k];
+        int indx=0;
+        for (int i=highestFreq-1;i>=0;i--)
+        {
+            if(bucketList[i]==null)continue;
+            for(int num:bucketList[i])
+            {
+                result[indx++]=num;
+                if(indx==k)return result;
             }
         }
-        
-        // Step 5: Convert to array
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return result;
+    }
+    static {
+        int[] input = {1, 1, 2, 2, 3};
+        for (int i = 0; i < 200; i++) {
+            topKFrequent(input, 2);
+        }
     }
 }
